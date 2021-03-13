89c9f12dff26c745eb7a4c49fc36ef547128dddd
RESUL COŞKUN <resulcoskun@aof.anadolu.edu.tr>
2021-03-13 17:02:32 +0300

    blockchair news
    blockchair üzerinden türkçe coin haberleri çekme

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-function sayfayaBaglan($_URL){ 
+<?php function sayfayaBaglan($_URL){ 
 	$useragent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; tr-TR; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13';
 	$referer = 'http://www.google.com/'; 
 	$ch = curl_init(); 
@@ -19,3 +19,12 @@ function ara($bas, $son, $yazi)
     '(.*?)'. preg_quote($son, '/').'/i', $yazi, $m);
     return @$m[1];
 }
+
+$siteAdresi = sayfayaBaglan("https://blockchair.com/tr/news");
+
+$baslik = ara('class="news__block__item_title">', "</a>", $siteAdresi);
+$newslink = ara('</a> <a href="', '" target="_blank"', $siteAdresi);
+echo "Haber Adı: ".$baslik[0]."<br>";
+echo "Haber Adı: ".$newslink[0]."<br>";
+
+?>
