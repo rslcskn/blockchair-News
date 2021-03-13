c036d572e047419af86299b91e85cd577c7e8db1
RESUL COŞKUN <resulcoskun@aof.anadolu.edu.tr>
2021-03-13 17:00:40 +0300

    Ara Fonksiyonu
    Curl içinde ara fonksiyonu

--- index.php
+++ index.php
@@ -12,3 +12,10 @@ function sayfayaBaglan($_URL){
 		curl_close($ch); 
 		return $rmx; 
 } 
+
+function ara($bas, $son, $yazi)
+{
+    @preg_match_all('/' . preg_quote($bas, '/') .
+    '(.*?)'. preg_quote($son, '/').'/i', $yazi, $m);
+    return @$m[1];
+}
